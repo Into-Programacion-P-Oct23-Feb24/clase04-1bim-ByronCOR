298e46239fa4b3495c41cac8e5111db9439e4a54
Byron Cordova <biaroncr@gmail.com>
2023-11-01 15:33:51 -0500

    cambios

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -10,11 +10,17 @@ package ejemplo01;
  * @author reroes
  */
 public class Ejemplo04 {
+
     public static void main(String[] args) {
+        /*
+       DIFERECIA ENTRE:
+        println: cuando ejecutas permite que las variables a mostra pantalla tenga un salto de linea
+        print: permite que la variables a presentar a pantalla salgan todo junto sin salto de linea
+         */
         String nombreEstudiante = "René Rolando";
         String apellidoEstudiante = "Elizalde Solano";
         int nacimiento = 1983;
-        System.out.print(nombreEstudiante + " ");
+        System.out.println(nombreEstudiante + " ");
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
